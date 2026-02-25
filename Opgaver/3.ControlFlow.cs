b0d5a9e299f978aac22a06a5e251c90f9824fa0b
John <johnfriedli0985@gmail.com>
2026-02-25 12:58:20 +0100

    Update 3.ControlFlow.cs

--- Opgaver/3.ControlFlow.cs
+++ Opgaver/3.ControlFlow.cs
@@ -27,24 +27,53 @@ namespace Opgaver
                 "Lav et program som tjekker om en given værdi er højere eller lavere end 18"
             );
             // Lav opgaven herunder!
+            int number = int.Parse(Console.ReadLine());
+            if (number > 18)
+            Console.WriteLine("Tallet er større end 18");
+            else 
+            Console.WriteLine("Tallet er mindre end 18");
         }
 
         public static void If2()
         {
             Console.WriteLine("Lav et program som tjekker om en given værdi er lige eller ulige");
             // Lav opgaven herunder!
+             
+            int number = int.Parse(Console.ReadLine()); 
+            if (number % 2 == 0)
+                Console.WriteLine("number is even");
+            else 
+                Console.WriteLine("number is uneven");
+            
+                
+                
         }
 
         public static void Switch1()
         {
             Console.WriteLine("Lav et program som tjekker om en given værdi er lige eller ulige");
             // Lav opgaven herunder!
+            int number = int.Parse(Console.ReadLine());
+            switch (number % 2)
+            {
+            
+
+            case 0:
+            Console.WriteLine("number is even");
+            break;
+
+            case 1:
+            Console.WriteLine("number is uneven");
+            break;
         }
+    }
 
         public static void Ternary1()
         {
             Console.WriteLine("Lav et program som tjekker om en given værdi er lige eller ulige");
             // Lav opgaven herunder!
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(number % 2 == 0 ? "number is even" : "number is uneven");
         }
 
         public static void MiniProjektQuiz()
@@ -57,11 +86,21 @@ namespace Opgaver
             Console.WriteLine("Brugeren skal indtaste sit svar til hvert spørgsmål.");
             Console.WriteLine(
                 "Programmet skal tjekke, om svaret er rigtigt eller forkert, og til sidst udskrive, hvor mange rigtige brugeren fik."
-            );
+            ); 
             Console.WriteLine(
                 "Tip: Brug variabler til at gemme point og svar, og if/else til at tjekke svarene."
             );
             // Lav opgaven herunder!
+            Console.WriteLine("What color is the sky?");
+            if Console.ReadLine()?.ToLower() == "blue")
+            score++; 
+            Console.WriteLine("What color is the ocean?")
+            if (Console.ReadLine()?.ToLower() == "blue")
+                score++; 
+            Console.WriteLine("How old are you");
+            if (Console.ReadLine() == "40")
+                score++;
+             Console.WriteLine("You got the {score} out of 3.");
         }
 
         public static void MiniProjektKarakterFeedback()
@@ -70,7 +109,7 @@ namespace Opgaver
             Console.WriteLine("Opgave:");
             Console.WriteLine(
                 "Lav et program, hvor brugeren indtaster en karakter (fx 12, 10, 7, 4, 02, 00 eller -3)."
-            );
+            ); 
             Console.WriteLine(
                 @"Programmet skal give en passende feedback baseret på karakteren, 
             fx 'Super flot!', 'Godt klaret', 'Du kan gøre det bedre' osv."
